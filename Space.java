05d5aa8710a9e0cb463d27f6b6ac10de52348782
bitpit <ejr10@hampshire.edu>
2012-11-24 12:29:56 -0600

    first space attempt

---
+++ Space.java
@@ -0,0 +1,29 @@
+public class Space {
+    
+    private int value;
+    private int[] possiblities;
+    
+    
+    public Space (int dimensions) {
+     
+        value = 0;
+        possiblities = new int[dimensions];
+    }
+    
+    public int getPossibilities(){
+        return possiblities.length;
+    }
+    
+    public int getValue(){
+        return value;
+    }
+    
+    public static void main(String[] args){
+
+        //For testing
+        Space p = new Space(9);
+        System.out.println(""+p.getValue()+" cur val");
+        System.out.println(""+p.getPossibilities()+" no of possibliities");
+    }
+
+}
